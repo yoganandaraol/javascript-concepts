350918a75267c6a9ae2e3687fda1b5100ebb5203
Yogananda Rao Locherla <locherla@live.com>
2023-04-01 07:34:59 +0530

    Concept explained

--- 001-Closures.js
+++ 001-Closures.js
@@ -4,15 +4,25 @@
  */
 
 function outer() {
-  const name = 'John';
+  const name = 'Yoga';
   function inner() {
     console.log(`Hello ${name}!`);
   }
   return inner;
 }
 
-const innerFn = outer();
-innerFn(); // Output: Hello John!
-
+// To access the variable defined in outer function is possible
 // Attempting to access `name` variable outside of the `outer` function results in an error
-console.log(name); // Uncaught ReferenceError: name is not defined
+
+// PLAY
+// console.log(name); // Uncaught ReferenceError: name is not defined
+
+// To Solve this problem we can use closures concept in javascript
+// define an inner function in side outer function and return inner function reference.
+// Now by calling inner function we can access variable defined in outer function to accomplish intended behaviour\
+
+// PLAY
+const innerFn = outer();
+innerFn(); // Output: Hello Yoga!
+
+
